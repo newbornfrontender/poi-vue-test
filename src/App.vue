0b38c9cb05fd1ada7c4a80c1ecb6a8037540ff3c
Infant Frontender <newbornfrontender@gmail.com>
2018-10-11 13:18:01 +0300

    Добавил простую структуру

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    
+    <main>
+      <router-view/>
+    </main>
+  </section>
+</template>
